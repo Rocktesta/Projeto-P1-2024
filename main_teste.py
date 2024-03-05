d1b2afeac5babdea14bb0935622f95a6ab9da2d0
Victor Acioly <123516304+jvacioly@users.noreply.github.com>
2024-03-04 23:22:30 -0300

    Update main_teste.py
    Parte para trocar entre as animações do personagem está comentada

--- main_teste.py
+++ main_teste.py
@@ -1,6 +1,5 @@
 import pygame
 from player_script_teste import Player
-
 pygame.init()
 
 largura_tela = 1280
@@ -15,16 +14,19 @@ fps = 60
 BG = (152, 152, 152) 
 def draw_bg():
     tela.fill(BG)
+    pygame.draw.line(tela, (0, 0, 0), (0, 600), (largura_tela, 600))
 
 def play():
     pygame.display.set_caption('PLAY')
+
+    # Variáveis do jogo
+    gravidade = 0.75
 
     # Movimentações do Player
     mooving_left = False
     mooving_right = False
 
-    player = Player('player_Kiev', 200, 200, 3, 5)
-    inimigo = Player('inimigo_Aspirador', 600, 300, 3, 5)
+    player = Player('player_Kiev', 200, 600, 3, 5, gravidade)
 
     # Loop Principal do Jogo
     running = True
@@ -34,10 +36,18 @@ def play():
 
         draw_bg()
 
+        player.update_animacao()
         player.draw(tela)
-        player.move(mooving_left, mooving_right)
-        inimigo.draw(tela)
-
+        # updade das ações do player
+        '''
+        if player.alive:
+            if player.no_ar:
+                player.updade_action(2) # animação de pulo
+            elif mooving_left or mooving_right:
+                player.update_action(1) # animação de corrida
+            else:
+                player.update_action(0) # retorna para o idle'''
+        player.move(mooving_left, mooving_right) 
 
         for event in pygame.event.get():   # Loop para lidar com eventos
             if event.type == pygame.QUIT:
@@ -51,6 +61,8 @@ def play():
                     mooving_left = True
                 if event.key == pygame.K_d:
                     mooving_right = True
+                if event.key == pygame.K_SPACE and player.alive:
+                    player.jump = True
             # Soltar teclas no teclado
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_a:
