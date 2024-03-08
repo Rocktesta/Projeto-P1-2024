c07eb99090b40b8bd3bf685e4b9b4a7cacbf4fce
Victor Acioly <victoe.acioly@gmail.com>
2024-03-08 19:03:14 -0300

    Removendo arquivos inúteis

--- piso_script.py
+++
@@ -1,7 +0,0 @@
-import pygame
-
-
-class Piso:  #classe do jogador
-    def __init__(self, x, y, largura=2000, altura=10):  #inicializa o objeto jogador
-        self.rect = pygame.Rect(x, y, largura, altura)
-        
