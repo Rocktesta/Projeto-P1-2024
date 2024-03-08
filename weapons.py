b46dbbc91a4dfda2ab323227e2f07b9bd9956932
Jhse <joaohsebbers@gmail.com>
2024-03-08 13:06:59 -0300

    update

--- weapons.py
+++ weapons.py
@@ -1,40 +1,21 @@
 import pygame
+from pygame.sprite import _Group
+
 #Scrpits para as armas
 
-altura = 10
-largura = 20
-
-class Municao:
-    def __init__(self, x, y): # incializa objeto municao com quantidade = 10
-        self.rect = pygame.Rect(x, y,  40, 12)
-        self.qnt = 10
-
-class Pistol: #pistola
-    def __init__(self, x, y, altura, largura):
-        self.rect = pygame.Rect(x, y, 20, 10)
-
-    def update_position(self, x, y):
-        # Atualiza a posição da pistola para acompanhar o jogador
-        self.rect.x = x + 35  # Ajuste conforme necessário para a posição da pistola em relação ao jogador
-        self.rect.y = y + 5  # Ajuste conforme necessário para a posição da pistola em relação ao jogador
-
-    
-class Balas:
-    def __init__(self, x, y, tela):
-        self.rect_bala = pygame.Rect(x + 10, y, 10, 10)
+class shotgun(pygame.sprite.Sprite):
+    def __init__(self, tela, x, y, sprite, cooldown=1):
+        pygame.sprite.Sprite.__init__(self)
         self.tela = tela
-
-    def fogo(self, x, y, municao, player_dir):
-        if municao > 0 and player_dir == 'right':
-            self.rect_bala.x = x
-            self.rect_bala.y = y
-            pygame.draw.rect(self.tela, (255, 255, 255), self.rect_bala)
-            municao -= 1
-        elif municao > 0 and player_dir == 'left':
-            self.rect_bala.x = x - 200
-            self.rect_bala.y = y
-            pygame.draw.rect(self.tela, (255, 255, 255), self.rect_bala)
-            municao -= 1
-        return municao
-            
-
+        self.x = x
+        self.y = y
+        self.sprite = pygame.image.load(sprite).convert_alpha()
+        self.rect = pygame.rect(x, y, (self.sprite.get_width, self.sprite.get_height))
+        self.cooldown = cooldown
+        self.equip = False
+    def equip(self, equipada, player):
+        if equipada == True:
+            self.equip = True
+        self.rect.x = player.rect.x 
+        self.rect.y = player.rect.y
+        
